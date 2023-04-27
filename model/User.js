39826eb05b0195dfb4b682a2106b9ce42c72bf06
Luiz Fernando <luizfernando7254@gmail.com>
2023-04-27 13:18:09 -0300

    3º Commit

--- model/User.js
+++ model/User.js
@@ -7,14 +7,32 @@ const User = db.define('User', {
     type: DataTypes.STRING,
     required: true
   },
-  tipo: {
+  usuario: {
     type: DataTypes.STRING,
     required: true
   },
-  volumes: {
+  senha: {
     type: DataTypes.INTEGER,
     required: true
   }
+
+})
+
+const Pet = db.define('Pet',{
+    nome: {
+        type: DataTypes.STRING,
+        required: true
+      },
+      raça: {
+        type: DataTypes.STRING,
+        required: true
+      },
+      idade: {
+        type: DataTypes.INTEGER,
+        required: true
+      },
+
 })
 
 module.exports = User
+module.exports = Pet
