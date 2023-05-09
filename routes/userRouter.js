279ddd67633fca3e7e9698f07e75d657a3e9cb33
Luiz Fernando <luizfernando7254@gmail.com>
2023-05-09 08:53:12 -0300

    Correção de erros

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,25 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const FuncionarioController = require('../controllers/FuncionarioController')
-const PetController = require('../controllers/PetController')
 
-/*Rotas dos Funcionarios
-router.post('/addFunc', FuncionarioController. newFuncionarioSave)
-router.get('/allFunc', FuncionarioController.allFuncionario)
-
-//Rotas dos pets
-router.get ('/addPet', PetController.newPet)
-router.post ('/add', PetController.newPetSave)
-
-router.get('edit', PetController.updatePet)
-router.post('edit', PetController.updatePetSave)
-
-router.post('/remove', PetController.removePet)
-
-router.get ('/allPet', PetController.allPets)
-router.get ('/', PetController.home)*/
-
-module.exports = router
-
-/*router.get('/edit/:id', UserController.updateUser)
-router.post('/edit', UserController.updateUserSave)*/
+router.get('/edit/:id', UserController.updateUser)
+router.post('/edit', UserController.updateUserSave)
