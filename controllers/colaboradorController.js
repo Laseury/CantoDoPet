131820a47393cea6358e8e3495c4439093eb5b1c
Luiz Fernando <luizfernando7254@gmail.com>
2023-05-17 14:04:50 -0300

    Atualização dos Colaboradores

--- controllers/colaboradorController.js
+++ controllers/colaboradorController.js
@@ -29,6 +29,32 @@ module.exports = class ColaboradorController {
         const colaborador = await Colaborador.findAll({ raw: true })
         res.render('colaborador/viewColaborador', { colaborador })
     }
+
+    static async updateViewColaborador(req, res) {
+      const id = req.params.id
+      const colaborador = await Colaborador.findOne({ where: { id: id }, raw: true })
+      res.render('colaborador/editColaborador', { colaborador })
+    }
+
+    static async updateColaborador(req, res) {
+      const id = req.body.id
+      const colaborador = {
+          nome: req.body.nome,
+          sobrenome: req.body.sobrenome,
+          data_nascimento: req.body.dataNascimento,
+          cpf: req.body.cpf,
+          funcao: req.body.funcao,
+          login: req.body.login,
+          senha: req.body.senha
+      }
+      
+      await Colaborador.update(colaborador, { where: { id: id } })
+        .then(res.redirect('/colaborador/allColaborador'))
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
     static async removeColaborador(req, res) {
         const id = req.body.id
     
@@ -39,5 +65,7 @@ module.exports = class ColaboradorController {
             console.log(err)
           })
       }
+
+      
 }
 
--- routes/colaboradorRouter.js
+++ routes/colaboradorRouter.js
@@ -8,6 +8,8 @@ router.get('/add', ColaboradorController.newColaborador)
 router.post('/add', ColaboradorController.newColaboradorSave)
 router.post('/remove', ColaboradorController.removeColaborador)
 router.get('/allColaborador', ColaboradorController.allColaborador)
+router.get('/edit/:id', ColaboradorController.updateViewColaborador)
+router.post('/edit', ColaboradorController.updateColaborador)
 
 
 module.exports = router
