e4d1455f64dae23dfb903c89f2cbd91f672e67cd
Luiz Fernando <luizfernando7254@gmail.com>
2023-05-24 19:56:15 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -5,24 +5,23 @@
 //Instalar -> npm install mysql2 ou mysql
 
 //Importar o Sequelize
-const {Sequelize} = require('sequelize')
-
+const { Sequelize } = require('sequelize');
 
 //Passar o nome do banco, usuario e senha.
-const sequelize = new Sequelize('petshop', 'root', '',{
-    host: 'localhost',
+const sequelize = new Sequelize('petshop', 'root', '12345678', {
+  host: 'localhost',
 
-    //No dialect iremos dizer qual será o banco de dados que será utilizado
-    //Instalar o banco de dados -> npm install mysql
-    dialect: 'mysql',
-})
+  //No dialect iremos dizer qual será o banco de dados que será utilizado
+  //Instalar o banco de dados -> npm install mysql
+  dialect: 'mysql'
+});
 
 try {
-    sequelize.authenticate()
-    console.log("Banco de dados inicializado")
+  sequelize.authenticate();
+  console.log('Banco de dados inicializado');
 } catch (error) {
-    console.log('Banco de dados não conseguiu conectar', error)
+  console.log('Banco de dados não conseguiu conectar', error);
 }
 
 //Exportar o sequelize para que outras classes possam usar.
-module.exports = sequelize
+module.exports = sequelize;
